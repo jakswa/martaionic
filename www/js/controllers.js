4fb0cbfea5b82aa2d909e8e952836a261f82b4b6
Jake Swanson <jakswa@gmail.com>
2016-02-02 01:56:55 -0500

    geolocation + minor styling
    - geolocation only updates on controller init, could be a problem if all you do is
      hang out on the dashboard, still unsure how ionic/cordova treats being in the
    background
    - also using the normal browser GPS, when I thought I needed the cordova
      plugin... hopefully it works
    - TODO: clean this poo up

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,52 @@
-var mod = angular.module('starter.controllers', [])
-
-mod.controller('DashCtrl', function($scope, $state, Arrivals) {
+angular.module('starter.controllers', [])
+.controller('DashCtrl', function($scope, $state, Arrivals, $ionicPlatform) {
   $scope.arrivals = Arrivals.latestByStation();
   Arrivals.subscribe('arrivalsChanged', $scope);
   $scope.$on('arrivalsChanged', function() {
     $scope.arrivals = Arrivals.latestByStation();
+    if ($scope.savedPosition) {
+      $scope.nearbyStations = Arrivals.closestTo($scope.savedPosition, $scope.arrivals);
+    }
+  });
+
+  // geolocation
+  var savedPositionString = sessionStorage.getItem('position2');
+  if (savedPositionString) {
+    $scope.savedPosition = JSON.parse(savedPositionString);
+    $scope.nearbyStations = Arrivals.closestTo($scope.savedPosition);
+  }
+  $scope.geowait = true;
+  $ionicPlatform.ready(function() {
+    window.navigator.geolocation.getCurrentPosition(function(position) {
+      $scope.savedPosition = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      };
+      var newJson = JSON.stringify($scope.savedPosition);
+      var oldJson = sessionStorage.getItem('position2');
+      if (newJson != oldJson) {
+        sessionStorage.setItem('position2', newJson);
+      }
+      $scope.$apply(function() {
+        $scope.nearbyStations = Arrivals.closestTo($scope.savedPosition, $scope.arrivals);
+        $scope.geowait = false;
+      });
+    });
   });
 
   $scope.stationView = function(stationName) {
     $state.go("station", {stationName: stationName});
+  };
+
+  var dirOrder = ['n', 's', 'w', 'e'];
+  $scope.byDirection = function(obj) {
+    var res = {};
+    for (var i = 0; i < dirOrder.length; i++) {
+      if (obj[dirOrder[i]]) {
+        res[dirOrder[i]] = obj[dirOrder[i]];
+      }
+    }
+    return res;
   };
   
   $scope.timeDisplay = function(arrival) {
@@ -22,9 +60,9 @@ mod.controller('DashCtrl', function($scope, $state, Arrivals) {
     var seconds = parseInt(arrival.waiting_seconds);
     return ":" + parseInt(seconds / 60 + 1);
   };
-});
+})
 
-mod.controller('StationCtrl', function($scope, $state, Arrivals) {
+.controller('StationCtrl', function($scope, $state, Arrivals) {
   var stationName = $scope.stationName = $state.params.stationName + " station";
   $scope.arrivals = Arrivals.by('station', stationName);
   Arrivals.subscribe('arrivalsChanged', $scope);
@@ -36,9 +74,9 @@ mod.controller('StationCtrl', function($scope, $state, Arrivals) {
     $state.go("train", {trainId: trainId});
   };
   
-});
+})
 
-mod.controller('TrainCtrl', function($scope, $state, Arrivals) {
+.controller('TrainCtrl', function($scope, $state, Arrivals) {
   var trainId = $scope.trainId = $state.params.trainId;
   $scope.arrivals = Arrivals.by('train_id', trainId);
   Arrivals.subscribe('arrivalsChanged', $scope);
@@ -46,4 +84,3 @@ mod.controller('TrainCtrl', function($scope, $state, Arrivals) {
     $scope.arrivals = Arrivals.by('train_id', trainId);
   });
 });
-
